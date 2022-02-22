c7d0374036e3d0ca371c19021e592992e10ae709
djamelkorei <korei.djamel.eddine@gmail.com>
2022-02-22 12:30:10 +0100

    initial commit

---
+++ src/main/java/me/djamelkorei/springbootsecuritybasicauth/SpringBootSecurityBasicAuthApplication.java
@@ -0,0 +1,13 @@
+package me.djamelkorei.springbootsecuritybasicauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityBasicAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityBasicAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/djamelkorei/springbootsecuritybasicauth/SpringBootSecurityBasicAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package me.djamelkorei.springbootsecuritybasicauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityBasicAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
